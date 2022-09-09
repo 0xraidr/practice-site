1b86caeb944211baa360d54dfa85704d69f8230b
raidr <decentralizedju@gmail.com>
2022-09-09 08:43:17 -0400

    boop

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
     bg-gradient-to-t from-sec-blue via-main-blue to-black p-4 text-white'>
         <div className='flex flex-col p-4 justify-center
          max-w-screen-lg mx-auto h-full'>
-            <div className='pb-8'>
+            <div className='pb-8 text-lightest-slate'>
                 <h2 className='flex justify-center text-4xl font-bold'>04. Contact</h2>
                 <p className='py-6 flex justify-center'>Submit the form below to get in touch with me.</p>
             </div>
@@ -31,7 +31,7 @@ const Contact = () => {
                         focus:outline-none'
                          ></textarea>
 
-                         <button className='text-white bg-gradient-to-b from-cyan-500 to-blue-500
+                         <button className='text-white bg-gradient-to-tl from-main-blue to-regal-green
                           px-6 py-3 my-8 mx-auto flex items-center rounded-md
                            hover:scale-110 duration-300'>Let's Chat!</button>
                 </form>
